f4907ffa8351c335856911c8d389681f4a4e529e
wooshrow <s.w.b.prasetya@uu.nl>
2023-07-04 20:12:22 +0200

    adding unit test for timed until

--- src/test/java/eu/iv4xr/ux/pxtesting/ltl/Test_TimedUntil.java
+++ src/test/java/eu/iv4xr/ux/pxtesting/ltl/Test_TimedUntil.java
@@ -13,8 +13,7 @@ import static eu.iv4xr.framework.extensions.ltl.LTL.* ;
 
 public class Test_TimedUntil {
 	
-	
-	//@Test
+	@Test
 	public void test_eventually_within() {	// absolute-time eventually
 		
 		var tr = genHopeFearTrace(data_("hope",0,0,0,1,2),
@@ -35,13 +34,11 @@ public class Test_TimedUntil {
 		assertEquals(SATVerdict.SAT,  tr.satisfy(next(next(eventually_within(H(),40f,40f))))) ;	
 	}
 	
-	//@Test
+	@Test
 	public void test_until_within() { // absolute-time until
 		
 		var tr = genHopeFearTrace(data_("hope",0,0,0,1,2),
 				                  data_("fear",0,0,0,-1,-1)) ;
-		
-		until_within(now((XState S) -> S.fear() <= 0) , H(),0f,0f) ;
 		
 		assertEquals(SATVerdict.UNSAT, tr.satisfy(until_within(now(S -> S.hope() <= 0) , H(), null , 20f))) ;
 		assertEquals(SATVerdict.SAT, tr.satisfy(until_within(now(S -> S.hope() <= 0) , H(), null , 30f))) ;
@@ -88,5 +85,31 @@ public class Test_TimedUntil {
 			assertEquals(SATVerdict.SAT,tr.satisfy(next(next(eventually_rwithin(H(),20f,20f))))) ;
 		}
 	
+	@Test
+	public void test_until_rwithin() { // relative-time until
+			
+			var tr = genHopeFearTrace(data_("hope",0,0,0,1,2),
+					                  data_("fear",0,0,0,-1,-1)) ;
+			
+			assertEquals(SATVerdict.UNSAT, tr.satisfy(until_rwithin(now(S -> S.hope() <= 0) , H(), null , 20f))) ;
+			assertEquals(SATVerdict.SAT, tr.satisfy(until_rwithin(now(S -> S.hope() <= 0) , H(), null , 30f))) ;
+			assertEquals(SATVerdict.SAT, tr.satisfy(until_rwithin(now(S -> S.hope() <= 0) , H(), null , 40f))) ;
+
+			assertEquals(SATVerdict.UNSAT, 
+					tr.satisfy(next(until_rwithin(now(S -> S.hope() <= 0) , H(), null , 10f)))) ;
+			assertEquals(SATVerdict.SAT, 
+					tr.satisfy(next(until_rwithin(now(S -> S.hope() <= 0) , H(), null , 20f)))) ;
+
+			assertEquals(SATVerdict.SAT, tr.satisfy(until_rwithin(now(S -> S.hope() <= 0) , H(), 30f , 30f))) ;
+			assertEquals(SATVerdict.UNSAT, tr.satisfy(until_rwithin(now(S -> S.hope() <= 0) , H(), 40f , null))) ;
+
+			assertEquals(SATVerdict.SAT, 
+					tr.satisfy(next(until_rwithin(now(S -> S.hope() <= 0) , H(), 20f , 20f)))) ;
+			assertEquals(SATVerdict.UNSAT, 
+					tr.satisfy(next(until_rwithin(now(S -> S.hope() <= 0) , H(), 30f , null)))) ;
+
+			assertEquals(SATVerdict.UNSAT, 
+					tr.satisfy(next(next(until_rwithin(now(S -> S.hope() <= 0) , H(), 20f , 20f))))) ;
+		}
 
 }
